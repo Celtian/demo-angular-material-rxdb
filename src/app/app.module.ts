893f13dbba6c18ed212168b6351f8026da35bed3
Dominik Hladík <dominik.hladik@coex.cz>
2023-04-02 15:33:44 +0200

    chore(deps): deps added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxAppVersionModule } from 'ngx-app-version';
+import { VERSION } from 'src/environments/version';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { I18nModule } from './i18n';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    I18nModule.forRoot({
+      version: VERSION.version,
+      defaultLanguage: 'en',
+    }),
+    NgxAppVersionModule.forRoot({
+      version: VERSION.version,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+/* eslint-env es6 */
+/* eslint-disable */
+const { guessProductionMode } = require('@ngneat/tailwind');
+const colors = require('tailwindcss/colors');
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{html,ts,css,scss,sass,less,styl}'],
+  theme: {
+    colors: {
+      blue: colors.blue,
+      cyan: colors.cyan,
+      gray: colors.gray,
+      green: colors.green,
+      lime: colors.lime,
+      orange: colors.orange,
+      red: colors.red,
+      violet: colors.violet,
+      white: colors.white,
+      yellow: colors.yellow,
+      light: 'var(--color-light)',
+      'light-active': 'var(--color-light-active)',
+      'light-disabled': 'var(--color-light-disabled)',
+      dark: 'var(--color-dark)',
+      'dark-active': 'var(--color-dark-active)',
+      'dark-disabled': 'var(--color-dark-disabled)',
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
