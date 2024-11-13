5745633fedfb672b33f06341163153f4caf089ed
Dominik Hladík <dominik.hladik@seznam.cz>
2024-11-13 20:00:15 +0100

    chore(deps): minor deps updated

--- src/app/shared/constants/language.constant.ts
+++ src/app/shared/constants/language.constant.ts
@@ -2,7 +2,7 @@ import { Language, LanguageDto } from '../dto/language.dto';
 
 export const DEFAULT_LANGUAGE: Language = 'en';
 export const AVAILABLE_LANGUAGES: Language[] = Object.values(LanguageDto);
-export const LOCALES: { [key in LanguageDto]: string } = {
+export const LOCALES: Record<LanguageDto, string> = {
   cs: 'cs_CZ',
   en: 'en_US',
 };
