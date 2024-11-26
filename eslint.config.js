4b9184780c5ef9183153de82749361e1fe005a9c
Dominik Hladík <dominik.hladik@seznam.cz>
2024-11-26 22:05:17 +0100

    chore(deps): deps updated

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,8 @@ module.exports = tseslint.config(
   {
     files: ['**/*.html'],
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
-    rules: {},
+    rules: {
+      '@angular-eslint/template/prefer-self-closing-tags': ['error'],
+    },
   },
 );
